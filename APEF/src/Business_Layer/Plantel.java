22f584ed6b6c485e696f1de2c4e4d617f6c0a9b5
Miguel Pinto <mcpinto98@gmail.com>
2013-11-28 00:53:01 +0000

    alterei list para set plantel

--- APEF/src/Business_Layer/Plantel.java
+++ APEF/src/Business_Layer/Plantel.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Business_Layer;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  *
@@ -16,5 +10,5 @@ public class Plantel {
     
     // Variaveis de Instancia
     private Treinador treinador;
-    private ArrayList<Jogador> jogadores;
+    private HashSet<Jogador> jogadores;
 }
