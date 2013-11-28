8d93dfabc3ed9ee696509c637039aa89ed4d7c59
dianalemos <demosbb@gmail.com>
2013-11-28 16:18:11 +0000

    Set da Agenda

--- APEF/src/Business_Layer/Agenda.java
+++ APEF/src/Business_Layer/Agenda.java
@@ -4,4 +4,24 @@ import java.util.HashSet;
 
 public class Agenda {
 	private HashSet<Jogo> jogos;
+        
+        public Agenda(){
+            this.jogos = new HashSet<>();
+        }
+        
+        public Agenda(Agenda a){
+            this.jogos = a.getJogos();
+        }
+
+        public HashSet<Jogo> getJogos() {
+            HashSet<Jogo> aux = new HashSet<Jogo>();
+            for(Jogo j: this.jogos) aux.add(j);
+            return aux;
+        }
+    
+        public void setJogos(HashSet<Jogo> jogo){
+            this.jogos = new HashSet<Jogo>();
+            for(Jogo j : jogo) this.jogos.add(j);
+        }
+    
 }
