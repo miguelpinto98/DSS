055cfc712d9316332e9010fb77b206f3c5d9c75e
Miguel Pinto <mcpinto98@gmail.com>
2013-11-30 17:44:48 +0000

    Retirada a class Resultados, por não achar fazer muito sentido, visto que um jogo tem os golos do escalao casa e do escalao de fora

--- APEF/src/Business_Layer/Jogo.java
+++ APEF/src/Business_Layer/Jogo.java
@@ -1,15 +1,44 @@
 package Business_Layer;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 public class Jogo {
-    private boolean realizado = false;
-    
-    private GregorianCalendar data;
-    private int hora;
+    private boolean realizado; /* Comeca a falso */
+    private GregorianCalendar dia; /* Dia e hora de jogo */
     private Campo campo;
     private Arbitro arbitro;
-    private Resultado resultado;
-    private Escalao casa;
-    private Escalao fora;
+    private Escalao ecasa;
+    private Escalao efora;
+    private int numGolosCasa;
+    private int numGolosFora;
+    private HashMap<Integer,Integer> goleadores;
+	
+    public Jogo() {
+    	this.realizado = false;
+    	this.dia = new GregorianCalendar();
+    	this.campo = new Campo();
+    	this.arbitro = new Arbitro();
+    	//this.ecasa = new Escalao();
+    	//this.efora = new Escalao();
+    	this.numGolosCasa = 0;
+    	this.numGolosFora = 0;
+    	this.goleadores = new HashMap<> ();
+    }
+    
+    public Jogo(Jogo jogo) {
+	}
+    
+	public boolean equals(Object obj) {
+
+		return true;
+	}
+	
+	protected Jogo clone() {
+		return new Jogo(this);
+	}
+
+	public String toString() {
+		return "";
+	}
 }
--- APEF/src/Business_Layer/ResponsavelEscola.java
+++ APEF/src/Business_Layer/ResponsavelEscola.java
@@ -3,15 +3,44 @@ package Business_Layer;
 import java.util.HashSet;
 
 public class ResponsavelEscola extends Utilizador {
-	private HashSet<Equipa> equipas;
+	private HashSet<Equipa> equipas; //???
 	
+	public ResponsavelEscola(ResponsavelEscola re) {
+		super();
+		this.equipas = re.getEquipasResponsavel();
+	}
 	
-	public ResponsavelEscola(ResponsavelEscola responsavelEscola) {
+	public HashSet<Equipa> getEquipasResponsavel() {
+		HashSet<Equipa> hse = new HashSet<>();
+		
+		for(Equipa e : this.equipas)
+			hse.add(e.clone());
+		
+		return null;
 	}
 
+	public void setEquipasResponsavel(HashSet<Equipa> es) {
+		this.equipas = es;
+	}
 
 	public ResponsavelEscola clone() {
 		return new ResponsavelEscola(this);
 	}
 	
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if ((o == null) || (o.getClass() != this.getClass()))
+			return false;
+		else {
+			ResponsavelEscola re = (ResponsavelEscola) o;
+			return (this.getID() == re.getID());	
+		}
+	}
+	
+	public String toString() {
+		StringBuilder str = new StringBuilder("Responsavel Escola");
+		
+		return str.toString();
+	}
 }
