03c9c774b72cd53af75a4067387e49a8e8803961
serafimpinto <smcp92@gmail.com>
2013-12-02 16:27:22 +0000

    epoca-torneio

--- APEF/src/Business_Layer/Epoca.java
+++ APEF/src/Business_Layer/Epoca.java
@@ -1,17 +1,85 @@
 package Business_Layer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Epoca {
+    
+	//Variaveis de Instancia
     private String nome;
     private Campeonato campeonato;
-    private ArrayList<Torneio> torneios;
+    private HashSet<Torneio> torneios;
     
-    public Epoca(Epoca epoca) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Epoca clone() {
-    	return new Epoca(this);
+    //Construtores
+    public Epoca() {
+    	this.nome = "";
+    	this.campeonato = new Campeonato();
+    	this.torneios = new HashSet<>();
     }
+
+    public Epoca(Epoca e) {
+    	this.nome = e.getNome();
+    	this.campeonato = e.getCampeonato();
+    	this.torneios = e.getTorneios();
+    }
+
+    //Getters
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public Campeonato getCampeonato() {
+        return this.campeonato;
+    }
+    
+    public HashSet<Torneio> getTorneios() {
+        HashSet<Torneio> aux = new HashSet<Torneio>();
+        for(Torneio t: this.torneios) 
+            aux.add(t.clone());
+        return aux;
+        }
+
+    //Setters
+    public void setNome(String n) {
+        this.nome = n;
+    }
+
+    public void setCampeonato(Campeonato c) {
+        this.campeonato = c;
+    }
+    public void setTorneios(HashSet<Torneio> t) {
+        this.torneios = t;
+    }	
+
+    //Equals,hashCode,Clone,toString
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.torneios);
+        return hash;
+    }
+    
+    public boolean equals(Object o) {
+        if (this == null)
+            return true;
+        if ((o == null) || (this.getClass() != o.getClass()))
+            return false;
+        else {
+            Epoca t = (Epoca) o;
+            return ( this.nome.equals(t.getNome()) && this.campeonato.equals(t.getCampeonato()) &&
+        			this.torneios.equals(t.getTorneios()) );
+        }
+    }
+
+    public Epoca clone() {
+        return new Epoca(this);
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder(); 
+        
+        str.append("--Epoca--\n");
+        
+        return str.toString(); 
+	}
 }
--- APEF/src/Business_Layer/Torneio.java
+++ APEF/src/Business_Layer/Torneio.java
@@ -1,5 +1,15 @@
 package Business_Layer;
 
 public class Torneio {
+
+	public Torneio() {
+
+	}
+
+	public Torneio clone() {
+    	return new Torneio(this);
+    }
+
+
     
 }
