fb21aa7fac8bc2ce2e2a5db4d7f6b829bb70399f
maleite <maleite.cbc@gmail.com>
2013-12-03 00:28:56 +0000

    Revert "Criação de BancoDados"
    This reverts commit 7087c179d3ac1837c03fee1bec7d41ef1fbd72ee.

--- APEF/src/Business_Layer/APEF.java
+++ APEF/src/Business_Layer/APEF.java
@@ -3,22 +3,70 @@ package Business_Layer;
 import java.util.HashMap;
 
 public class APEF {
-	private BancoDados bd;
+	private HashMap<String, Escola> escola;
+	private HashMap<String, Epoca> epoca;
+	private HashMap<String, Utilizador> users;
+
+    public static int IDENTIFICADOR=1;
     
     public APEF() {
-    	this.bd = new BancoDados();
+    	this.escola = new HashMap<>();
+    	this.epoca = new HashMap<>();
+    	this.users = new HashMap<>();
     }
 
 	public APEF(APEF a) {
-		this.bd = a.getBancoDados();
+		this.escola = a.getEscolas();
+		this.epoca = a.getEpoca();
+		this.users = a.getUsers();
 	}
 
-	public BancoDados getBancoDados() {
-		return this.bd;
+	public HashMap<String, Escola> getEscolas() {
+		HashMap<String,Escola> aux = new HashMap<>();
+		
+		for(String s : this.escola.keySet())
+			aux.put(s, this.escola.get(s).clone());
+		
+		return aux;
+	}
+	
+	public HashMap<String, Epoca> getEpoca() {
+		HashMap<String,Epoca> aux = new HashMap<>();
+		
+		for(String s : this.epoca.keySet())
+			aux.put(s, this.epoca.get(s).clone());
+		
+		return aux;
 	}
 
-	public void setBancoDados(BancoDados bd) {
-		this.bd = bd;
+	public HashMap<String, Utilizador> getUsers() {
+		HashMap<String,Utilizador> aux = new HashMap<>();
+		
+		for(String s : this.users.keySet())
+			aux.put(s, this.users.get(s).clone());
+		
+		return aux;
+	}
+
+	public void setEscolas(HashMap<String, Escola> escolas) {
+		this.escola = escolas;
+	}
+	
+	public void setEpoca(HashMap<String, Epoca> epocas) {
+		this.epoca = epocas;
+	}
+	
+	public void setUsers(HashMap<String, Utilizador> users) {
+		this.users = users;
+    }
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.epoca == null) ? 0 : this.epoca.hashCode());
+		result = prime * result + ((this.escola == null) ? 0 : this.escola.hashCode());
+		result = prime * result + ((this.users == null) ? 0 : this.users.hashCode());
+		return result;
 	}
 
 	public boolean equals(Object o) {
@@ -28,7 +76,7 @@ public class APEF {
 			return false;
 		else {
 			APEF apef = (APEF) o;
-			return this.bd.equals(apef.getBancoDados());
+			return this.escola.equals(apef.getEscolas()) && this.epoca.equals(apef.getEpoca()) && this.users.equals(apef.getUsers());
 		}
 	}
 	
@@ -40,7 +88,6 @@ public class APEF {
 		StringBuilder str = new StringBuilder(); 
 		
 		str.append("APEF");
-		str.append(this.getBancoDados().toString());
 		
 		return str.toString(); 
 	}
