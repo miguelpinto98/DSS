ff3f058bc294876605f3ccf39b4dd4c4ff937450
Diana Lemos <demosbb@gmail.com>
2014-01-06 19:38:26 +0000

    Jtab Torneios

--- APEF/src/GUI/Torneio/JOptions.java
+++ APEF/src/GUI/Torneio/JOptions.java
@@ -7,6 +7,7 @@
 package GUI.Torneio;
 
 import Business_Layer.Utilizador;
+import GUI.Home2;
 
 /**
  *
@@ -14,9 +15,9 @@ import Business_Layer.Utilizador;
  */
 public class JOptions extends javax.swing.JPanel {
 
-    private JPrincipalTorneio p;
+    private Home2 p;
     
-    public JOptions(JPrincipalTorneio p, Utilizador u) {
+    public JOptions(Home2 p, Utilizador u) {
         initComponents();
         this.p = p;
     }
@@ -34,14 +35,21 @@ public class JOptions extends javax.swing.JPanel {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
+        setBackground(new java.awt.Color(255, 255, 255));
         setBorder(javax.swing.BorderFactory.createTitledBorder("Gerir torneios"));
         setMinimumSize(new java.awt.Dimension(940, 83));
         setPreferredSize(new java.awt.Dimension(940, 83));
-        setLayout(new java.awt.BorderLayout());
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setMinimumSize(new java.awt.Dimension(699, 60));
 
+        jButton2.setBackground(new java.awt.Color(255, 255, 255));
         jButton2.setText("Criar torneio");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Inscrever equipa");
 
@@ -54,7 +62,7 @@ public class JOptions extends javax.swing.JPanel {
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(239, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -66,8 +74,21 @@ public class JOptions extends javax.swing.JPanel {
                 .addContainerGap())
         );
 
-        add(jPanel1, java.awt.BorderLayout.CENTER);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
